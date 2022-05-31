497af9b580f7dfb118d4e9395dbb9285519be6af
TairSadyk <tairsadykov92@gmail.com>
2022-05-31 18:00:37 -0400

    Popup component with JS functionality as added (finished Desktop version of the website)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
-
-
+sectionPricing = document.querySelector(".section-pricing");
+iconClose = document.querySelector(".popup__icon-close");
+popupCont = document.querySelector(".popup");
+popupContent = document.querySelector(".popup__content");
+sectionPricing.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.closest(".btn--white")) popupCont.classList.remove("hidden");
+  popupContent.classList.add("scale-to-normal");
+});
+iconClose.addEventListener("click", function () {
+  popupCont.classList.add("hidden");
+  popupContent.classList.remove("scale-to-normal");
+});
